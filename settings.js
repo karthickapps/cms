7e323c72b93272bc6fde613f5000b411323a549a
karthick <karthick.k@smartandgeek.com>
2016-05-12 23:57:11 +0530

    mongo uri

--- settings.js
+++ settings.js
@@ -1,3 +1,3 @@
 module.exports = {
-	database : "mongodb://heroku_jbhlv86r:heroku_jbhlv86r@ds021462.mlab.com:21462/heroku_jbhlv86r"
+	database : "mongodb://heroku_jbhlv86r:qf7r7a1vq1p80ii55qp09ju7hk@ds021462.mlab.com:21462/heroku_jbhlv86r"
 };
